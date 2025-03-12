6dbd470177b0afcc32e2f39de4f6986ad1a5e684
Weverton Rocha <wevertonrocha@yahoo.com>
2025-03-11 22:07:49 -0300

    Exercícios restantes da lista 1'

---
+++ Ex01.java
@@ -0,0 +1,6 @@
+public class Ex01 {
+    public static void main(String[] args) {
+    String nome = "Weverton";
+        System.out.println("Meu nome é " + nome);
+    }
+}
---
+++ Ex02.java
@@ -0,0 +1,8 @@
+public class Ex02 {
+    public static void main(String[] args) {
+        String nome = "Weverton";
+        int idade = 26;
+        double altura = 1.78;
+        System.out.println("Meu nome é " + nome + ", tenho " + idade + " anos e " + altura + "m de altura.");
+    }
+}
